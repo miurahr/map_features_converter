4be365fe5c5cf14abdc1c41ed3dd3a992e4074f6
Hiroshi Miura <miurahr@linux.com>
2012-10-14 22:29:37 +0900

    change properties key rule
    - now don't add tag such as feature, input
     but use <id name>.<attribute> as property key.

--- src/jp/osmf/mfc/MapFeaturesFilter.java
+++ src/jp/osmf/mfc/MapFeaturesFilter.java
@@ -14,15 +14,13 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.AttributesImpl;
 
-public final class MapFeaturesFilter extends XMLFilterImpl {    
-
-    public enum featureCategory {NONE, CATEGORY, INPUTSET, FEATURE};
+public final class MapFeaturesFilter extends XMLFilterImpl {
 
     private Properties configuration;
     private Stack <String>elements;
+    private Stack <String>featureKeys;
     private String featureKey;
     private String elemName;
-    private featureCategory featureCat;
 
     private boolean help_processed = false;
 
@@ -39,9 +37,9 @@ public final class MapFeaturesFilter extends XMLFilterImpl {
     public void startDocument() throws SAXException
     {
         elements = new Stack<String>();
+        featureKeys = new Stack<String>();
         featureKey = null;
         elemName = null;
-        featureCat = featureCategory.NONE;
         super.startDocument();
     }
 
@@ -52,6 +50,7 @@ public final class MapFeaturesFilter extends XMLFilterImpl {
 
     private AttributesImpl replaceAtts(AttributesImpl newatts, String featureKey, String tag) {
         String localizedName = configuration.getProperty(featureKey + "." + tag);
+        System.out.println(featureKey+"."+tag+":"+newatts.getValue(newatts.getIndex(tag)); // debug
         if (localizedName != null) {
             newatts.setValue(newatts.getIndex(tag), localizedName);
         }
@@ -80,53 +79,42 @@ public final class MapFeaturesFilter extends XMLFilterImpl {
      * @throws SAXException
      */
     public final void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException {
-        elements.push(qName);
+
+        elements.push(qName); // trace all elements on stack
 
         for (int i=0;i<atts.getLength();i++) {
             String aname = atts.getQName(i);
             if (aname.equals("id")) {
-                if (qName.equals("category")) {
-                    featureCat = featureCategory.CATEGORY;
-                    featureKey = "category." + atts.getValue(i);
-                    elemName = qName;
-                } else if (qName.equals("inputSet")) {
-                    featureCat = featureCategory.INPUTSET;
-                    featureKey = "input." + atts.getValue(i);
-                    elemName = qName;
-                } else if (qName.equals("feature")) {
-                    featureCat = featureCategory.FEATURE;
-                    featureKey = "feature." + atts.getValue(i);
-                    elemName = qName;
-                }
+                featureKey = atts.getValue(i);
+                featureKeys.push(featureKey); // trace all attribute keys on stack
+                elemName = qName;
             }
         }
 
         AttributesImpl newatts = new AttributesImpl(atts);
         if (featureKey != null) {
-            if (elemName == qName) {
-                if (featureCat == featureCategory.CATEGORY) {
+            if (elemName == qName) { // when element that have "id" attribute
+                if (elemName.equals("category")) {
                     newatts = replaceAtts(newatts, featureKey, "name");
-                } else if (featureCat == featureCategory.FEATURE) {
+                } else if (elemName.equals("feature")) {
                     newatts = replaceAtts(newatts, featureKey, "name");
                 }
-            } else if (featureCat == featureCategory.INPUTSET) {
-                if (qName.equals("input")) {
-                    String inputKey = getAttsValue(newatts, "key");
-                    if (inputKey != null) {
-                        featureKey = featureKey + "." + inputKey;
-                        newatts = replaceAtts(newatts, featureKey, "name");
-                        newatts = replaceAtts(newatts, featureKey, "description");
-                    }
-                } else if (qName.equals("choice")) {
-                    String choiceKey = getAttsValue(newatts, "value");
-                    if (choiceKey != null) {
-                        newatts = replaceAtts(newatts, featureKey + "." + choiceKey, "text");
-                    }
+            } else if (qName.equals("input")) {
+                String inputKey = getAttsValue(newatts, "key");
+                if (inputKey != null) {
+                    featureKeys.push(featureKey);
+                    elemName = qName;
+                    featureKey = featureKey + "." + inputKey;
+                    newatts = replaceAtts(newatts, featureKey, "name");
+                    newatts = replaceAtts(newatts, featureKey, "description");
                 }
-            } else if (featureCat == featureCategory.FEATURE) {
-                if (qName.equals("icon")) {
-                    newatts = replaceAtts(newatts, featureKey + ".icon", "image");
+            } else if (qName.equals("choice")) {
+                String choiceKey = getAttsValue(newatts, "value");
+                if (choiceKey != null) {
+                    newatts = replaceAtts(newatts, featureKey + "." + choiceKey, "text");
                 }
+            } else if (qName.equals("icon")) {
+                newatts = replaceAtts(newatts, featureKey + ".icon", "image");
             }
         }
         super.startElement(uri, localName, qName, newatts);
@@ -139,24 +127,18 @@ public final class MapFeaturesFilter extends XMLFilterImpl {
      * @throws SAXException
      */
     public final void characters(char[] ch, int start, int length) throws SAXException {
-        StringBuffer buf = new StringBuffer();
-        String currentElem = elements.peek();
 
-        if (currentElem.equals("help")) {
+        if ("help".equals(elements.peek())) {
             String helpuri = configuration.getProperty(featureKey + ".help");
             if (helpuri != null && !help_processed) {
-                // replace chars to newchars
-                char[] newch = helpuri.toCharArray();
-                super.characters(newch, 0, newch.length);
                 help_processed = true;
-            } else {
-                // just pass to output
-                super.characters(ch, start, length);
+                char newch[] = helpuri.toCharArray();
+                super.characters(newch, 0, newch.length);
+                return;
             }
-        } else {
-            // just pass to output
-            super.characters(ch, start, length);
         }
+        super.characters(ch, start, length);
+        return;
     }
 
     /**
@@ -168,13 +150,12 @@ public final class MapFeaturesFilter extends XMLFilterImpl {
     public final void endElement(String uri, String localName, String qName) throws SAXException {
         String popedElem = elements.pop();
         if (elemName != null) {
-            if (featureCat == featureCategory.FEATURE && elemName.equals("help") && popedElem.equals("help")) {
-                help_processed = false;
-            }
             if (elemName.equals(popedElem)) {
+                if (elemName.equals("help")) {
+                    help_processed = false;
+                }
                 elemName = null;
-                featureKey = null;
-                featureCat = featureCategory.NONE;
+                featureKey = featureKeys.pop();
             }
         }
         super.endElement(uri, localName, qName);
